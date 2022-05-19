c33a4d940518b68dde105f62fc13000cd721822e
lili618 <>
2022-05-19 10:50:03 +0000

    add Variable

--- main.cs
+++ main.cs
@@ -2,6 +2,15 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+    bool bVariable = true;
+    int iVariable = 10;
+    float fVariable = 3.4f;
+    char cVariable = 'a';
+    
+    Console.WriteLine (bVariable);
+    Console.WriteLine (iVariable);
+    Console.WriteLine (fVariable);
+    Console.WriteLine (cVariable);
   }
 }
