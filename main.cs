2a98cca602842d965fcade1189195eb610f3d9f7
JiNi <76019082+OhYeonjin@users.noreply.github.com>
2022-05-19 11:17:40 +0000

    add for sentance

--- main.cs
+++ main.cs
@@ -3,12 +3,13 @@ using System;
 class Program {
   public static void Main (string[] args) {
 
-    int num = 8;
-    if (num == 10)
-      Console.WriteLine("입력된 값이 10 입니다.");
-    else if (num == 9)
-      Console.WriteLine("입력된 값이 9 입니다.");
-    else
-      Console.WriteLine("입력된 값이 9와 10이 아닙니다.");
+    int i = 1;
+    while (true)
+    {
+      Console.WriteLine(i++);
+  
+      if(i > 10)
+        break;
+    }
   }
 }
