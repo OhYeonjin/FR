58923e3c17e2095155b564473d70969f78501221
JiNi <76019082+OhYeonjin@users.noreply.github.com>
2022-05-19 11:47:43 +0000

    practice class & instance

--- main.cs
+++ main.cs
@@ -1,12 +1,24 @@
 using System;
 
+class Person {
+  public string Name;
+  public string Brithday;
+  
+
+  public void Eat(){
+    Console.WriteLine(Name + "(이)가 식사를 합니다.");
+  }
+  
+}
+
 class Program {
   public static void Main (string[] args) {
 
-    sbyte value1 = 64;
-    sbyte value2 = 64;
-    int value3 = (int)value1 + value2;
-    Console.WriteLine(value3);
+    Person p1 = new Person();
+
+    p1.Name = "서준";
+
+    p1.Eat();
     
   }
 }
