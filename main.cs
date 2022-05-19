ce616323c75e3a71266bb16d4d4e42a6d2969591
JiNi <76019082+OhYeonjin@users.noreply.github.com>
2022-05-19 11:54:24 +0000

    add method & instance

--- main.cs
+++ main.cs
@@ -1,24 +1,35 @@
 using System;
 
-class Person {
+class Person 
+{
   public string Name;
-  public string Brithday;
   
-
-  public void Eat(){
+  public void Eat()
+  {
     Console.WriteLine(Name + "(이)가 식사를 합니다.");
+  }
+
+  public void Run()
+  {
+    Console.WriteLine(Name + "(이)가 뜁니다.");
   }
   
 }
 
-class Program {
-  public static void Main (string[] args) {
+class Program 
+{
+  public static void Main (string[] args) 
+  {
 
     Person p1 = new Person();
-
-    p1.Name = "서준";
-
-    p1.Eat();
+    Person p2 = new Person();
     
+    p1.Name = "서준";
+    p2.Name = "다윤";
+    
+    p1.Eat();
+    p1.Run();
+    p2.Eat();
+    p2.Run();
   }
 }
